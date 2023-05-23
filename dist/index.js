b3ef39436781285ca9d79077d9bd5daceb761e81
vikas-cldcvr <vikas@cldcvr.com>
2023-05-23 21:27:29 +0530

    slack message format updated 3

--- dist/index.js
+++ dist/index.js
@@ -130,7 +130,7 @@ function run() {
                 const releaseNotes = yield extractReleaseNotes(packageFilesPath[packageWithVersion[0]], 'false');
                 const md = `\nPackage : \`${packageWithVersion[0]}@${packageWithVersion[1]}\`\n${releaseNotes}\nTo update your package to the latest version, simply run the following command in your project directory:\n\`npm install ${packageWithVersion[0]}@${packageWithVersion[1]}\`\n\nIf you're using Yarn, you can use the following command:\n\n\`yarn add ${packageWithVersion[0]}@${packageWithVersion[1]}\`\n`;
                 const mrkdwn = (0, slackify_markdown_1.default)(md.trim());
-                changeLogs += `${mrkdwn.trim()}\n`;
+                changeLogs += `${mrkdwn.trim()}\n\n`;
             }
             // eslint-disable-next-line no-console
             console.info(`${changeLogs}`);
--- src/main.ts
+++ src/main.ts
@@ -77,7 +77,7 @@ async function run(): Promise<void> {
       const md = `\nPackage : \`${packageWithVersion[0]}@${packageWithVersion[1]}\`\n${releaseNotes}\nTo update your package to the latest version, simply run the following command in your project directory:\n\`npm install ${packageWithVersion[0]}@${packageWithVersion[1]}\`\n\nIf you're using Yarn, you can use the following command:\n\n\`yarn add ${packageWithVersion[0]}@${packageWithVersion[1]}\`\n`
       const mrkdwn = slackifyMarkdown(md.trim())
 
-      changeLogs += `${mrkdwn.trim()}\n`
+      changeLogs += `${mrkdwn.trim()}\n\n`
     }
     // eslint-disable-next-line no-console
     console.info(`${changeLogs}`)
