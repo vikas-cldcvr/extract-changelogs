acc7894aff4ccebe5b02a65358ce9736c7bed574
vikas-cldcvr <vikas@cldcvr.com>
2023-05-23 20:43:43 +0530

    gitaction added

---
+++ SAMPLE_CHANGELOG.md
@@ -0,0 +1,54 @@
+<h4 class="margin-btm-8">Release Notes</h4>
+<hr class="margin-btm-32" />
+
+# Change Log
+
+## [1.14.5] - 2023-05-17
+
+### Features
+- Slack notifications GitAction integrated
+### Improvements
+- Unnecessary badges removed from README.md
+
+## [1.14.4] - 2023-05-16
+
+### Bug Fixes
+
+- `f-select` opening issue fixed.
+
+## [1.14.3] - 2023-05-16
+
+### Improvements
+
+- `f-checkbox` && `f-radio` label click added
+## [1.14.2] - 2023-05-16
+
+### Improvements
+
+- `f-select` : selected value must display using optionTemplate if available.
+- `disabled` property support added to option object 
+
+## [1.14.1] - 2023-05-16
+
+### Bug fixes
+
+- `f-accordion` : `header hover state is fixed.
+- `f-switch`: removed the buggy outline on focus, and removed the extra gaps occurrence that was happening when no slots were present.
+
+## [1.14.0] - 2023-05-15
+
+### Bug fixes
+
+- `f-select` : `optionTemplate` converted from string to function to fix security hotspots.
+
+## [1.13.3] - 2023-05-03
+
+### Bug fixes
+
+- `f-select` : setting `clear` prop to false.
+
+## [1.13.2] - 2023-05-03
+
+### Bug fixes
+
+- `z-index` not resetting in child `f-popover`.
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,85 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import fs from 'fs'
+import readline from 'readline'
+import slackifyMarkdown from 'slackify-markdown'
+
+const encoding = 'utf8'
+const eol = '\n'
+const topEmptyLines = new RegExp(`^([${eol}]*)`, 'm')
+
+async function extractReleaseNotes(
+  changelogFile: fs.PathLike,
+  prerelease: string
+): Promise<string> {
+  const fileStream = fs.createReadStream(changelogFile, {encoding})
+  const rl = readline.createInterface({
+    input: fileStream
+  })
+  const lines = []
+  let inside_release = false
+  for await (const line of rl) {
+    const start_of_release =
+      !!line.match('^#+ \\[[0-9]') ||
+      (prerelease === 'true' && !!line.match('^#+ \\[Unreleased\\]'))
+    if (inside_release) {
+      if (start_of_release) {
+        core.debug(`next version found: '${line}'`)
+        break
+      } else {
+        lines.push(line)
+        core.debug(`add line: '${line}'`)
+      }
+    } else {
+      if (start_of_release) {
+        inside_release = true
+        core.debug(`version found: '${line}'`)
+      } else {
+        core.debug(`skip line: '${line}'`)
+      }
+    }
+  }
+  let releaseNotes = lines.reduce(
+    (previousValue, currentValue) => previousValue + eol + currentValue
+  )
+  releaseNotes = trimEmptyLinesTop(releaseNotes)
+  return releaseNotes
+}
+
+function trimEmptyLinesTop(releaseNotes: string): string {
+  return releaseNotes.replace(topEmptyLines, '')
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const packageFilesPathString: string = core.getInput(
+      'changelog-files-config'
+    )
+    const packageVersionsString: string = core.getInput(
+      'released-package-versions'
+    )
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const packageFilesPath = JSON.parse(packageFilesPathString)
+    const packageVersions = JSON.parse(packageVersionsString)
+    let changeLogs = ``
 
-    core.setOutput('time', new Date().toTimeString())
+    for (const packageWithVersion of Object.entries(packageVersions)) {
+      const releaseNotes = extractReleaseNotes(
+        packageFilesPath[packageWithVersion[0]],
+        'false'
+      )
+
+      const md = `
+			${packageWithVersion[0]}@${packageWithVersion[1]}\n
+			${releaseNotes}\n
+			To update your package to the latest version, simply run the following command in your project directory:\n
+			\`npm install ${packageVersions[0]}@${packageVersions[1]}\`\n\nIf you're using Yarn, you can use the following command:\n\n\`yarn add ${packageVersions[0]}@${packageVersions[1]}\`\n
+			`
+      const mrkdwn = slackifyMarkdown(md)
+
+      changeLogs += mrkdwn
+    }
+
+    core.setOutput('changeLogs', changeLogs)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
